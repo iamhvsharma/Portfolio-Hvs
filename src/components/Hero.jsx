f952ae824080394ea3875a3c12f37a3d7816e2b8
iamhvsharma <itshvsharma@gmail.com>
2024-05-04 15:00:50 +0530

    Hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,15 +12,15 @@ const Hero = () => {
         <div className="flex flex-row justify-center items-center w-1/2 sm:w-1/2">
           <img
             className="rounded-full"
-            src="https://github.com/rohan-kulkarni-25.png"
+            src="https://avatars.githubusercontent.com/u/123243221?v=4"
           />
         </div>
-        <div className="w-1/2 flex flex-col gap-16 sm:w-full sm:gap-8">
+        <div className="w-1/2 flex flex-col gap-14 sm:w-full sm:gap-8">
           <span className="text-4xl sm:text-2xl">
             &mdash; Hello, I'm Harshvardhan 👋
           </span>
           <div className="flex flex-col gap-4">
-            <span className="text-8xl  font-bold sm:text-5xl">Software</span>
+            <span className="text-8xl  font-bold sm:text-5xl">Full-Stack</span>
             <span className="font-bold text-8xl font-mono sm:text-5xl">
               {" "}
               Developer
@@ -31,7 +31,7 @@ const Hero = () => {
           </div>
           <div className="flex flex-row gap-12 ">
             <a
-              href="https://drive.google.com/file/d/1ZNf5yIXKoX9VSCdWXewLq2xqRtvdMTUE/view?usp=sharing"
+              href="https://drive.google.com/file/d/1L3nJT6ckN0rm7ROjzAki7SwaxOKt0ppM/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
               className="border-blue-300 border hover:bg-blue-500 hover:text-white  p-4 rounded-2xl text-xl shadow-xl sm:text-sm sm:p-3"
@@ -39,7 +39,7 @@ const Hero = () => {
               📄 Download Resume
             </a>
             <a
-              href="mailto:rohank2502@gmail.com"
+              href="mailto:itshvsharma@gmail.com"
               target="_blank"
               rel="noreferrer"
               className="border-blue-300 border hover:bg-blue-500 hover:text-white  p-4 rounded-2xl text-xl shadow-xl sm:text-sm sm:p-3"
@@ -49,28 +49,28 @@ const Hero = () => {
           </div>
           <div className="text-5xl flex flex-row gap-8 sm:text-3xl">
             <a
-              href="https://twitter.com/rohan_2502"
+              href="https://twitter.com/iamhvsharma"
               target="_blank"
               rel="noreferrer"
             >
               <FaSquareXTwitter />
             </a>
             <a
-              href="https://www.linkedin.com/in/rohan-k-2502/"
+              href="https://www.linkedin.com/in/iamhvsharma"
               target="_blank"
               rel="noreferrer"
             >
               <FaLinkedin />
             </a>
             <a
-              href="https://www.instagram.com/rohankulkarni.dev/"
+              href="https://www.instagram.com/iamharshvardhan9"
               target="_blank"
               rel="noreferrer"
             >
               <AiFillInstagram />
             </a>
             <a
-              href="https://github.com/rohan-kulkarni-25"
+              href="https://github.com/iamhvsharma"
               target="_blank"
               rel="noreferrer"
             >
@@ -80,10 +80,10 @@ const Hero = () => {
         </div>
       </div>
       <div className="bg-yellow-200 w-fit mx-auto p-8 rounded-2xl text-center text-2xl sm:text-lg sm:text-justify sm:rounded-none sm:w-full">
-        Fusing Passion with Community: Co-Creating Experiences at
-        <span className="font-bold italic"> React India </span>
-        and Sparking Innovation with
-        <span className="font-bold italic"> Hack The League</span>.
+        Learning and Implementing Web Development to build 
+        <span className="font-bold italic"> Products </span>
+        which helps people 
+        <span className="font-bold italic"> to make their life easyy</span>.
       </div>
     </div>
   );
