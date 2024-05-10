d922b774d1baaaaf094baa610a0055f595b93550
iamhvsharma <itshvsharma@gmail.com>
2024-05-10 11:25:01 +0530

    Experience Done

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -27,32 +27,22 @@ const Experiences = () => {
           
           </p>
         </div>
-        {/* <div className="border border-gray-400 w-3/4 flex-row flex place-content-between p-4 rounded-2xl gap-24">
+         <div className="border border-gray-400 w-3/4 flex-row flex place-content-between p-4 rounded-2xl gap-24">
           <div className="flex flex-col gap-1  justify-center items-center font-bold w-96">
-            <span className="text-2xl">SDE Intern</span>
+            <span className="text-2xl">Indie Hacker</span>
             <p className="text-center text-lg text-gray-500">
-              Cloudware Solutions LLC
+              Developing Products
             </p>
             <span className="text-sm italic text-gray-500">
-              Jan 2023 - Sept 2023
+              April 2024 - Present
             </span>
           </div>
 
           <p className="text-left text-lg text-gray-500 w-full">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            modi optio officia fugit consequuntur earum dolore voluptatum,
-            molestias animi quisquam dolorum vero incidunt corporis
-            reprehenderit ipsa repudiandae saepe ipsam, deleniti eaque debitis.
-            Illo modi reprehenderit totam, ex repellat distinctio iure accusamus
-            repellendus dignissimos eum, est incidunt. Eius corporis blanditiis
-            facere non ullam, consequatur error quidem incidunt! Similique
-            repellendus commodi aut qui eius architecto in cumque, fugiat a
-            animi sequi ab officia soluta sit maiores. Reprehenderit sint cum
-            vitae rem, sit corrupti eum maxime ipsum enim reiciendis nesciunt
-            deserunt aliquid temporibus saepe ratione totam repudiandae. Est,
-            officiis error. Corrupti, officia similique!
+            I am working as a Indie Hacker to build my own products which are developed to solve a particular problem.
+            We are planning to launch our first Application in June END. 
           </p>
-        </div> */}
+        </div>
       </div>
     </div>
   );
