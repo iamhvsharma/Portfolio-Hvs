30dbe2888b02a03a9ab22aa3f60c6ca4fe000b32
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-03-24 02:03:34 +0530

    Shadcn added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,83 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-
-    },
-    screens: {
-      '2xl': { 'max': '1535px' },
-      // => @media (max-width: 1535px) { ... }
-
-      'xl': { 'max': '1350px' },
-      // => @media (max-width: 1279px) { ... }
-
-      'lg': { 'max': '1400px' },
-      // => @media (max-width: 1023px) { ... }
-
-      'md': { 'max': '900px' },
-      // => @media (max-width: 767px) { ... }
-
-      'sm': { 'max': '750px' },
-      //
-    },
-    extend: {
-      fontFamily: {
-        mona_sans: ["Mona-Sans"],
-      },
-    },
+  	fontFamily: {},
+  	screens: {
+  		'2xl': {
+  			max: '1535px'
+  		},
+  		xl: {
+  			max: '1350px'
+  		},
+  		lg: {
+  			max: '1400px'
+  		},
+  		md: {
+  			max: '900px'
+  		},
+  		sm: {
+  			max: '750px'
+  		}
+  	},
+  	extend: {
+  		fontFamily: {
+  			mona_sans: [
+  				'Mona-Sans'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
